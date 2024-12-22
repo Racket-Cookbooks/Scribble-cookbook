3efa9cd20cf3d174bda1822af57335389f28b4a3
Stephen De Gabrielle <spdegabrielle@users.noreply.github.com>
2024-12-22 18:41:03 +0000

    Create info.rkt

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "scribblecookbook")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/scribblecookbook.scrbl" ())))
+(define pkg-desc "Description scribblecookbook")
+(define version "1.0")
+(define pkg-authors '(stephendegabrielle))
+(define license '(Apache-2.0 OR MIT))
